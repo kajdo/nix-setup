383172a99fafb17c07a92244e608c47da7c23d63
Juergen Kajdocsy <juergen.kajdocsy@gmail.com>
2025-01-23 00:17:37 +0100

    battery enhancement for laptop

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -53,13 +53,38 @@ in {
     LC_TIME = "de_AT.UTF-8";
   };
 
-  # Enable the X11 windowing system.
+   # Enable the X11 windowing system.
    services.xserver.enable = true;
    services.xserver.resolutions = [ { x = 1920; y = 1080; } ];
-   # services.xserver.libinput.naturalScrolling = true;
    services.libinput.touchpad.naturalScrolling = true;
+   services.xserver.videoDrivers = [ "modesetting" ];
 
-  # Enable the LXQT Desktop Environment.
+  # Battery life improvement
+  # Better scheduling for CPU cycles - thanks System76!!!
+  services.system76-scheduler.settings.cfsProfiles.enable = true;
+
+  # Enable TLP (better than gnomes internal power manager)
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+  };
+
+  # Disable GNOMEs power management
+  services.power-profiles-daemon.enable = false;
+
+  # Enable powertop
+  powerManagement.powertop.enable = true;
+
+  # Enable thermald (only necessary if on Intel CPUs)
+  services.thermald.enable = true;
+
+
+
    services.xserver.displayManager.lightdm.enable = true;
    #services.xserver.desktopManager.lxqt.enable = true;
 
@@ -92,16 +117,7 @@ in {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kajdo = {
