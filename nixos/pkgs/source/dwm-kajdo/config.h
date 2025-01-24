a86a1c3f496d8753ba0e3538f3c8e719f6ba5c3d
Juergen Kajdocsy <juergen.kajdocsy@gmail.com>
2025-01-24 23:20:12 +0100

    updated to latest build

--- nixos/pkgs/source/dwm-kajdo/config.h
+++ nixos/pkgs/source/dwm-kajdo/config.h
@@ -130,7 +130,7 @@ viewprev(const Arg *arg) {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 /*static const char *termcmd[]  = { "alacritty", NULL };*/
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *scratchcmd[]  = { "scratch", NULL };
 static const char *wallpaperchangecmd[]  = { "set_random_wallpaper.sh", NULL };
 static const char *autostartcmd[]  = { "dwm_autostart_cmd.sh", NULL };
@@ -169,7 +169,7 @@ static const Key keys[] = {
 	/*{ MODKEY,                       XK_Tab,    view,           {0} },*/
 	{ MODKEY,                       XK_Escape,    view,           {0} },
 	{ MODKEY,	                XK_q,      killclient,     {0} },
-	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask,             XK_n,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
@@ -180,6 +180,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+    	{ MODKEY|ShiftMask,             XK_j,      pushdown,       {0} },
+	{ MODKEY|ShiftMask,             XK_k,      pushup,         {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
