2077c09d40fdfd4420b709b69e5f1976fe82f5d9
Juergen Kajdocsy <juergen.kajdocsy@gmail.com>
2025-05-26 12:13:24 +0200

    lock fix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,20 +2,17 @@
  {
    inputs = {
      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-     # nixvim.url = "github:nix-community/nixvim";
    };
- 
-   # outputs = { self, nixpkgs, nixvim, ... } @ inputs: {
+
    outputs = { self, nixpkgs, ... } @ inputs: {
      nixosConfigurations = {
        nixos = nixpkgs.lib.nixosSystem {
          system = "x86_64-linux";
- 
+
          modules = [
            ./configuration.nix
-           # nixvim.nixosModules.nixvim
          ];
- 
+
          # Pass inputs to the module system
          specialArgs = { inherit inputs; };
        };
