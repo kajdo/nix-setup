36d898317adf8e7dd8e68badc6c4d292ca162cc3
Juergen Kajdocsy <juergen.kajdocsy@gmail.com>
2025-07-01 01:02:11 +0200

    flake update because of error during opencode testing

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,23 +1,22 @@
 # /etc/nixos/flake.nix
-{
+ {
    inputs = {
      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-     readest-web-flake = {
-       url = "path:./flakes/readest-web";
-       # --- THIS IS THE FIX ---
-       # This tells Nix to provide our `nixpkgs` flake input
-       # as an input also named `nixpkgs` to the sub-flake.
-       inputs.nixpkgs.follows = "nixpkgs";
-     };
+     # readest-flake.url = "path:./flakes/readest";
+     readest-web-flake.url = "path:./flakes/readest-web";
    };
+
   outputs = { self, nixpkgs, readest-web-flake, ... } @ inputs: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+
         modules = [
           ./configuration.nix
+          # readest-flake.nixosModules.readest
           readest-web-flake.nixosModules.readest-web
         ];
+
         specialArgs = { inherit inputs; };
       };
     };
