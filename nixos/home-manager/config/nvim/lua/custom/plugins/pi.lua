ac7941b1fa5f294000d132eff58c07b999490e40
Juergen Kajdocsy <juergen.kajdocsy@gmail.com>
2026-04-03 12:05:04 +0200

    disabled pi.lua for now

--- nixos/home-manager/config/nvim/lua/custom/plugins/pi.lua
+++ nixos/home-manager/config/nvim/lua/custom/plugins/pi.lua
@@ -1,10 +1,11 @@
 return {
-  -- "pablopunk/pi.nvim",
-  dir = "/home/kajdo/git/pi.nvim",
-  config = function()
-    require("pi").setup()
+	enabled = false,
+	-- "pablopunk/pi.nvim",
+	dir = "/home/kajdo/git/pi.nvim",
+	config = function()
+		require("pi").setup()
 
-    vim.keymap.set("n", "<leader>ai", ":PiAsk<CR>", { desc = "Ask pi" })
-    vim.keymap.set("v", "<leader>ai", ":PiAskSelection<CR>", { desc = "Ask pi (selection)" })
-  end,
+		vim.keymap.set("n", "<leader>ai", ":PiAsk<CR>", { desc = "Ask pi" })
+		vim.keymap.set("v", "<leader>ai", ":PiAskSelection<CR>", { desc = "Ask pi (selection)" })
+	end,
 }
