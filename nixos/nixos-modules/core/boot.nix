3ee1ec95327696e931ac2fb7637efc009c90a514
Juergen Kajdocsy <juergen.kajdocsy@gmail.com>
2026-07-01 23:50:33 +0200

    changed kernel params to have a chance of logs in case citrix remote connect freezes the machine / or reboot after 10s

--- nixos/nixos-modules/core/boot.nix
+++ nixos/nixos-modules/core/boot.nix
@@ -13,6 +13,13 @@
     "net.ipv6.conf.default.disable_ipv6" = true;
     "net.ipv6.conf.lo.disable_ipv6" = true;
     "net.ipv6.conf.wlp4s0.disable_ipv6" = true;
+
+    # --- Capture a hard GPU/system lockup instead of dying silently ---
+    "kernel.nmi_watchdog"     = 1;   # ARM the NMI hardlockup detector (was 0/disarmed!)
+    "kernel.hardlockup_panic" = 1;   # NMI-detected hard lockup -> panic
+    "kernel.softlockup_panic" = 1;   # CPU stuck in kernel >20s -> panic
+    "kernel.panic_on_oops"    = 1;   # kernel oops -> panic (instead of limping on)
+    "kernel.panic"            = 10;  # reboot 10s after any panic
   };
 
   # Fix USB devices sleeping after 2-3 seconds
